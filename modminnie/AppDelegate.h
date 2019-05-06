6d6101026f16c8c178dbadda0402950130a9d531
Taylor Murray <murratay@umich.edu>
2019-05-06 16:48:21 -0400

    Initial Commit

---
+++ modminnie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  modminnie
+//
+//  Created by Taylor Murray on 5/6/19.
+//  Copyright © 2019 Taylor Murray. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
